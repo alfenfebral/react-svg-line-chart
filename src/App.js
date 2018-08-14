5d0d5441afc2df55f1bbf1e0ef365b92d124bc8b
Alfen Febral <alfenfebral@gmail.com>
2018-08-14 18:17:43 +0700

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React, { Component } from "react";
+import "./App.css";
+import LineChart from './LineChart';
+
+class App extends Component {
+  createFakeData() {
+    // This function creates data that doesn't look entirely random
+    const data = [];
+    for (let x = 0; x <= 30; x++) {
+      const random = Math.random();
+      const temp = data.length > 0 ? data[data.length - 1].y : 50;
+      const y =
+        random >= 0.45
+          ? temp + Math.floor(random * 20)
+          : temp - Math.floor(random * 20);
+      data.push({ x, y });
+    }
+    return data;
+  }
+  render() {
+    return (
+      <div className="App">
+        <LineChart data={this.createFakeData()} />
+      </div>
+    );
+  }
+}
+export default App;
